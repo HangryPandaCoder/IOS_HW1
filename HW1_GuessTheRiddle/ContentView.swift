6a31c3bd8f38073c6f2861c9d6e7dc76e17407e9
wei shan <ycc7688@gmail.com>
2021-03-08 22:42:35 +0800

    Initial Commit

---
+++ HW1_GuessTheRiddle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW1_GuessTheRiddle
+//
+//  Created by 葉之霆 on 2021/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW1_GuessTheRiddle/HW1_GuessTheRiddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW1_GuessTheRiddleApp.swift
+//  HW1_GuessTheRiddle
+//
+//  Created by 葉之霆 on 2021/3/8.
+//
+
+import SwiftUI
+
+@main
+struct HW1_GuessTheRiddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
